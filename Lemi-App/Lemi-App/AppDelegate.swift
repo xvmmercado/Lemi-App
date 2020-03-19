c0b02e3fc60593542cb0f6baceff67b78af8ee5e
xandervince <alexjazel@gmail.com>
2020-03-19 18:19:41 +0800

    added cocoapods

--- Lemi-App/Lemi-App/AppDelegate.swift
+++ Lemi-App/Lemi-App/AppDelegate.swift
@@ -11,11 +11,19 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
+    lazy var dashboardController = DisplayDashboardViewController()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        self.showDashboard()
         return true
+    }
+    
+    fileprivate func showDashboard() {
+        DispatchQueue.main.async {
+            self.window?.rootViewController = UINavigationController(rootViewController: self.dashboardController)
+        }
     }
 }
 
---
+++ Lemi-App/Pods/Target Support Files/Pods-Lemi-App/Pods-Lemi-App-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Lemi_App : NSObject
+@end
+@implementation PodsDummy_Pods_Lemi_App
+@end
