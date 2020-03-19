42118bd3b0e484eea785484b872280dc663c00da
Alexander Vincent Mercado <62352759+xvmmercado@users.noreply.github.com>
2020-03-19 10:45:45 +0800

    Add files via upload

---
+++ Lemi-App/Lemi-App/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Lemi-App
+//
+//  Created by Xander Mercado on 3/19/20.
+//  Copyright © 2020 Xander Mercado. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ Lemi-App/Lemi-App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lemi-App
+//
+//  Created by Xander Mercado on 3/19/20.
+//  Copyright © 2020 Xander Mercado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
